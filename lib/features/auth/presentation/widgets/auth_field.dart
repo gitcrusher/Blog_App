ef7782d15b3fcbae6879c0fde8448dd64c265926
gitcrusher <aayushguptasoni@gmail.com>
2024-08-03 20:47:13 +0530

    commit

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
-  const AuthField({super.key});
+  final String hintText;
+  const AuthField({
+    super.key,
+    required this.hintText,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: hintText,
+      ),
+    );
   }
 }
